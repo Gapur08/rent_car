66aefeca5af457ad0d1ac47eb704fc653e659e2c
Gapur08 <gapurowmuhammetali@gmail.com>
2025-04-25 10:39:50 -0700

    1.0.6

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,23 @@
 <?php
 
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\CarControllerController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/cars', function () {
     return "hello world";
 });
 
-//Route::middleware('guest')
-//    ->group(function () {
-//        Route::get('register', [RegisterController::class, 'create'])->name('register');
-//        Route::post('register', [RegisterController::class, 'store']);
-//    });
+Route::controller(CarController::class)
+    ->prefix('cars')
+    ->name('cars.')
+    ->group(function () {
+        Route::get('', 'index')->name('index');
+    });
+
+Route::middleware('guest')
+    ->group(function () {
+        Route::get('register', [RegisterController::class, 'create'])->name('register');
+        Route::post('register', [RegisterController::class, 'store']);
+    });
